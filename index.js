2e1bcd10b5c3d6af12d9d40c3c190f6c521f44dd
Luis Angel Reyes Mendoza <zs24022197@estudiantes.uv.mx>
2026-03-23 07:53:37 -0600

    Ayuda y Depurador agregados al menu

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const {app, BrowserWindow} = require ('electron');
+const {app, BrowserWindow, Menu} = require ('electron');
 const { readSync } = require('fs');
+const menu = require ('./menu');
 
 let window; 
 
-app.on('ready',    () => {
+app.on('ready', () => {
     window = new BrowserWindow ({
-        width: 1920,
-        height: 1200,
+        width: 1280,
+        height: 720,
         webPreferences: {
             nodeIntegration: true
         }
     });
     window.loadFile('index.html');
-})
+});
+
+Menu.setApplicationMenu(menu);
--- menu.js
+++ menu.js
@@ -1,15 +1,32 @@
-const {menu,shell} = require ('electron');
+const { Menu, shell } = require('electron'); 
 
 const template = [
     {
+        label: 'Ayuda',
         role: 'help',
         submenu: [
             {
-                label: 'About Editor Component',
+                label: 'Acerca del componente Editor',
                 click() {
-                    shell.openExternal('https://simplemde.com/');
+                    shell.openExternal('https://simplemde.com');
                 }
+            }
+        ]
+    },
+    {
+        label: 'Depurador',
+        submenu: [
+            {
+                label: 'Herramientas de Desarrollador',
+                role: 'toggleDevTools'
             }
         ]
     }
 ];
+
+
+ const menu = Menu.buildFromTemplate(template);
+
+module.exports = menu;
+
+
