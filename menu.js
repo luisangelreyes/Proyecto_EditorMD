edb527151af1cfeed28d4dd9b5a95676c9618add
Luis Angel Reyes Mendoza <zs24022197@estudiantes.uv.mx>
2026-03-22 22:19:06 -0600

    Añadí menú y correccion de ventana en SimpleMDE

---
+++ menu.js
@@ -0,0 +1,15 @@
+const {menu,shell} = require ('electron');
+
+const template = [
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'About Editor Component',
+                click() {
+                    shell.openExternal('https://simplemde.com/');
+                }
+            }
+        ]
+    }
+];
